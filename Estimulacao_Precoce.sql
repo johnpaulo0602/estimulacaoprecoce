f435eb0c19c467d009f4062851fe0177fdc03e25
Grifths <lucaszmp011@gmail.com>
2023-05-19 15:20:06 -0300

    Atualização BD e Integração do BD

--- Estimulacao_Precoce.sql
+++ Estimulacao_Precoce.sql
@@ -6,11 +6,14 @@ USE Estimulacao_Precoce;
 CREATE TABLE Alunos(
   IdAluno int primary key not null auto_increment,
   NomeCompleto varchar (50) not null,
-  Telefone1 varchar(20) not null,
-  Telefone2 varchar(20) not null,
   CPF varchar (20) not null unique,
   DataNascimento date not null,
   Sexo varchar (15) not null,
+  Mae varchar (40) not null,
+  Pai varchar (40) not null,
+  Matricula varchar (30) not null,
+  Telefone1 varchar(20) not null,
+  Telefone2 varchar(20) not null,
   Naturalidade varchar (20),
   UF varchar (5) not null,
   HipoteseDiagnostica varchar(800) not null
@@ -19,29 +22,28 @@ CREATE TABLE Alunos(
 -- Criando tabela Turma com informações das turmas
 
 CREATE TABLE Turma(
-  IdTurma int primary key not null auto_increment,
-  Identificacao varchar (30) not null,
+  NomeTurma varchar (30) primary key not null,
   Turno varchar (20) not null,
   Professor varchar (50) not null
 );
 
 -- Criando tabela AlunoTurma com informações de alunos associados a turmas
 
+
 CREATE TABLE AlunoTurma (
   IdAluno int not null,
-  IdTurma int not null,
-  primary key (IdAluno, IdTurma),
+  NomeTurma_al varchar(30) not null,
+  primary key (IdAluno, NomeTurma_al),
   foreign key (IdAluno) references Alunos(IdAluno) ON DELETE CASCADE,
-  foreign key (IdTurma) references Turma(IdTurma) ON DELETE CASCADE
+  foreign key (NomeTurma_al) references Turma(NomeTurma) ON DELETE CASCADE
 );
 
 -- Criando tabela Professor com informações dos professores
 
 CREATE TABLE Professor(
-  IdProfessor int primary key not null auto_increment,
-  NomeCompleto varchar(50) not null,
-  IdTurma int not null,
-  foreign key (IdTurma) references Turma(IdTurma)
+  NomeCompleto varchar(50) primary key not null,
+  NomeTurma_Pr varchar(30) not null,
+  foreign key (NomeTurma_Pr) references Turma(NomeTurma)
 );
 
 -- Criando tabela Endereco com informações dos endereços
--- cadastro.py
+++ cadastro.py
@@ -2,14 +2,16 @@ from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 
 # Função responsável por coletar os dados cadastrados dos alunos.
+
+dados_endereco = []
 dados_alunos = []
 dados_turma = []
 
 def cadastrar_dados_alunos():
     
-    campos_line_edit = ['line_nome','line_cpf','line_matricula','line_mae',
+    campos_line_edit = ['line_nome','line_cpf','Line_Edit_Dt','line_mae',
                         'line_pai','line_telefone1','line_telefone2','line_naturalidade',
-                        'Line_Edit_Dt'] 
+                        'line_matricula'] 
 
     dados_line_edit = [janela.findChild(QtWidgets.QLineEdit, campo).text() for campo in campos_line_edit]
 
@@ -19,25 +21,34 @@ def cadastrar_dados_alunos():
 
     dados_text_edit = janela.findChild(QtWidgets.QTextEdit,'line_hipotese').toPlainText()
 
-    dados = dados_line_edit + dados_combo_box + [dados_text_edit]
-    dados_alunos.append(dados)
+    campos_line_edit_endereco = ['line_cidade','line_numero','line_uf','line_endereco','line_cep']
 
-    print(dados_alunos)
+    dados_line_edit_endereco = [janela.findChild(QtWidgets.QLineEdit,campo).text() for campo in campos_line_edit_endereco]
 
-def cadastrar_dados_endereco():
-    campos_line_edit = ['line_cidade','line_numero','line_uf','line_endereco','line_cep']
 
-    
-
-    if '' in dados:
+    # Verificar se algum campo está vazio
+    if '' in dados_line_edit or '' in dados_combo_box or dados_text_edit == '' or '' in dados_line_edit_endereco:
         QMessageBox.about(janela, 'Alerta', 'Algum campo está vazio')
-    else:
-        for campo in campos_line_edit:
-            janela.findChild(QtWidgets.QLineEdit, campo).setText('')
-        for campo in campos_combo_box:
-            janela.findChild(QtWidgets.QComboBox, campo).setCurrentIndex(0)
-        janela.findChild(QtWidgets.QTextEdit, 'line_hipotese').clear()
+        return  # Retorna sem adicionar os dados
+    
+    dados_endereco.append(dados_line_edit_endereco)
 
+    dados = dados_line_edit + dados_combo_box + [dados_text_edit]
+
+    dados_alunos.append(dados)
+    for campo in campos_line_edit:
+            janela.findChild(QtWidgets.QLineEdit, campo).setText('')
+
+    for campo in campos_line_edit_endereco:
+            janela.findChild(QtWidgets.QLineEdit, campo).setText('')
+
+    for campo in campos_combo_box:
+        janela.findChild(QtWidgets.QComboBox, campo).setCurrentIndex(0)
+    janela.findChild(QtWidgets.QTextEdit, 'line_hipotese').clear()
+
+    for campo in campos_line_edit:
+        janela.findChild(QtWidgets.QLineEdit, campo).setText('')
+    
 
 #Função responsável por coletar os dados referente a turma
 def cadastrar_turma():
@@ -51,10 +62,7 @@ def cadastrar_turma():
     turno_combo_box = [janela.findChild(QtWidgets.QComboBox, campo).currentText() for campo in combo_box_turma]
 
     dados = turno_combo_box + dados_line_edit
-    dados_turma.append(dados)
-
-    print (dados_turma)
-    
+    dados_turma.append(dados)  
 
     if '' in dados:
         QMessageBox.about(janela, 'Aletar', 'Algum campo está vazio')
@@ -64,17 +72,12 @@ def cadastrar_turma():
         for campo in combo_box_turma:
             janela.findChild(QtWidgets.QComboBox, campo).setCurrentIndex(0)
 
-
-
-
-
 #deletar os dados
 def deletar_dados():
     for line in janela.findChildren(QtWidgets.QLineEdit):
         line.clear()
     janela.findChild(QtWidgets.QTextEdit, 'line_hipotese').clear()
-
-                
+  
 #executador da interface
 app=QtWidgets.QApplication([])
 janela=uic.loadUi('estimulacaoprecoce/interface.ui')
@@ -85,3 +88,5 @@ janela.show()
 app.exec()
 
 print (dados_turma)
+print (dados_alunos)
+print (dados_endereco)
